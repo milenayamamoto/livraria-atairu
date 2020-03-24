550cfc06d20cdc8e2a332035ce7b383f0becc452
Milena Yamamoto <milenayamamoto@gmail.com>
2020-03-24 20:59:44 -0300

    Criando base para pedidos

--- routes/web.php
+++ routes/web.php
@@ -11,33 +11,28 @@
 |
 */
 
+//Rotas views
 Route::get('/', 'HomeController@index');
-Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/cadastro', 'HomeController@cadastro');
-Route::get('/carrinho', 'HomeController@carrinho');
-
 Route::get('/contato', 'HomeController@contato');
 Route::get('/faq', 'HomeController@faq');
 Route::get('/login', 'HomeController@login');
 Route::get('/sobre', 'HomeController@sobre');
 
+//CRUD usuários
 Route::any('/conta', 'UserController@index')->name('conta.index');
 Route::post('/conta/alterarConta', 'UserController@update')->name('conta.update');
 Route::get('/conta/deletarConta', 'UserController@delete')->name('conta.delete');
+Route::any('/auth/register', 'HomeController@index');
+
+//CRUD pedidos
+Route::get('/login/pedidos', 'PedidoController@index');
+Route::get('/carrinho', 'HomeController@carrinho');
+
 
 Route::get('/produtos', 'ProdutoController@index');
 Route::get('/produto/{id}', 'ProdutoController@show');
-Route::any('/auth/register','HomeController@index');
 
+//Autenticação
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
