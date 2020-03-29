7e4d12678515b7ea0bc608e096507db7af6d0894
Milena Yamamoto <milenayamamoto@gmail.com>
2020-03-29 16:36:53 -0300

    Refatoração da home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Produto;
 
 class HomeController extends Controller
 {
@@ -22,8 +23,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $novidades = Produto::orderBy('created_at', 'desc')->take(7)->get(); //Carousel Novidades
+
+        return view('home', compact('novidades'));
     }
+
+    // TODO: Carousel Ultimo ano, add no banco e seguir link
+    // public function carouselUltimoAno()
+    // {
+    //     https://www.nytimes.com/2019/11/22/books/review/best-books.html
+    // }
+
+    // TODO: Carousel Mulheres - Pegar lista
+    // public function carouselMulheres()
+    // {
+    // }
 
     public function contato()
     {
